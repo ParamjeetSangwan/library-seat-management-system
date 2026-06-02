94af62cfe90f2dcae0e7089d7c9ae7d0f287f8dd
Paramjeet <Paramjeetsangwan678@gmail.com>
2026-06-02 14:14:54 +0530

    Added homepage route

--- backend/urls.py
+++ backend/urls.py
@@ -2,8 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({
+        "status": "ok",
+        "message": "Library Backend Running"
+    })
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
